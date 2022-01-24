8d224d38fb81fdd06fdb34523e0dab5331dd49b6
timsonner <timsonner@gmail.com>
2022-01-24 05:29:34 +0000

    Add update method

--- src/lib/components/RowView.svelte
+++ src/lib/components/RowView.svelte
@@ -1,10 +1,26 @@
 <script>
-    export let todo
+    export let todo, updateTodo
 </script>
 
-<div>
+<div class="todo">
     <input type="checkbox" checked={todo.completed}>
-    <input type="text" value={todo.task}>
-    <button>❌</button>
-    <button>📝</button>
+    
+    <!-- on input, call updateTodo() -->
+    <!-- event.currentTarget is this input -->
+    <!-- updates the task property of todo -->
+    <input 
+        type="text" 
+        value={todo.task}
+        on:input={(event) => {
+            todo.task = event.currentTarget.value
+            updateTodo(todo)}}>
+
+    <button>Edit</button>
+    <button>Delete</button>
 </div>
+<style>
+    .todo {
+        display: flex;
+        margin-bottom: 0.25em;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,45 @@
 <script>
-import { onMount } from "svelte";
-import { supabase } from '$lib/supabaseClient'
-import { each } from "svelte/internal";
-import RowView from '$lib/components/RowView.svelte'
+    import { onMount } from "svelte";
+    import { supabase } from "$lib/supabaseClient";
+    import RowView from "$lib/components/RowView.svelte";
 
-let todos = []
+    let todos = [];
+    let loading = true;
 
     onMount(async () => {
-let {data, error} = await supabase.from('todos').select('*')
-if (error) {
-    alert(error)
+        let { data, error } = await supabase.from("todos").select("*");
+        if (error) {
+            alert(error);
+        }
+        todos = data;
+        console.table(todos);
+        loading = false;
+    });
+
+const updateTodo = async (todo) => {
+    console.table(todo)
+    // .update updates the 'task' column with the current todo.task
+    // .eq grabs the correct db entry by matching the current task.id to an entry with the same id in the 'id' column
+    try {
+        const { data, error } = await supabase
+        .from('todos')
+        .update({ task: todo.task })
+        .eq('id', todo.id)
+
+    } catch (error) {
+        alert(error.value)
+    }
 }
-todos = data
-console.table(todos)
-    })
+
 </script>
 
-{#each todos as todo}
-    
-<RowView {todo} />
-{/each}
+{#if !loading}
+    {#each todos as todo}
+        <!-- pass props to the RowView component -->
+        <RowView {todo} {updateTodo}/>
+    {:else}
+        <p>No todos found</p>
+    {/each}
+{:else}
+    <p>Loading todos...</p>
+{/if}
