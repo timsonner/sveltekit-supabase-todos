63653d0c65b303ee006911fc320d1f1d5fb5c8a8
timsonner <timsonner@gmail.com>
2022-01-24 02:03:17 +0000

    Base UI and SupaBase integration

---
+++ src/lib/components/RowView.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let todo
+</script>
+
+<div>
+    <input type="checkbox" checked={todo.completed}>
+    <input type="text" value={todo.task}>
+    <button>❌</button>
+    <button>📝</button>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+import { onMount } from "svelte";
+import { supabase } from '$lib/supabaseClient'
+import { each } from "svelte/internal";
+import RowView from '$lib/components/RowView.svelte'
+
+let todos = []
+
+    onMount(async () => {
+let {data, error} = await supabase.from('todos').select('*')
+if (error) {
+    alert(error)
+}
+todos = data
+console.table(todos)
+    })
+</script>
+
+{#each todos as todo}
+    
+<RowView {todo} />
+{/each}
